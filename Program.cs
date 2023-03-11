2f27638dbdc4b4fc0ad80536109f53bf1501cc04
Dennis-050 <gm0515032022@unab.edu.sv>
2023-03-10 20:06:40 -0600

    tarea7

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.Write("Introduce un número entero: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= i; j++) {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
